f02e5d47be4577ceb09995cecb7f97bf57cea240
haideraly001 <haideralymughal@gmail.com>
2025-03-28 10:46:42 +0500

    work with brevo smtp to

--- utility/email.js
+++ utility/email.js
@@ -3,26 +3,38 @@ import nodemailer from "nodemailer"
 
 const emailSender = async (option) => {
   try {
+    // mailTrap
+    // const transporter = nodemailer.createTransport({
+    //   host: "sandbox.smtp.mailtrap.io",
+    //   port: 2525,
+    //   secure: false,
+    //   auth: {
+    //     user: "88554b773b0dec",
+    //     pass: "eecd8ae41fab47",
+    //   },
+    // });
+
+    // brevo 
     const transporter = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
+      host: "smtp-relay.brevo.com",
+      port: 587,
       secure: false,
       auth: {
-        user: "88554b773b0dec",
-        pass: "eecd8ae41fab47",
+        user: "890eec001@smtp-brevo.com",
+        pass: "kDrmK3OaCLt7P5wN",
       },
     });
 
 
-    const info = await transporter.sendMail({
-      from: "haideralymughal01@gmail.com",
+    await transporter.sendMail({
+      from: "haideralymughal@gmail.com",
       to: option.sendEmail,
       subject: "Password reset Notification ",
       text: option.text,
       // html: "<b>follow the text</b>",
     });
 
-    console.log("success");
+    console.log("success email send");
   } catch (err) {
     console.log("err-->", err.message);
 
