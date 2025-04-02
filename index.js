6e97125e0f2005a71669aa0ce123ffa15d8ac3e3
haideraly001 <haideralymughal@gmail.com>
2025-04-02 23:56:43 +0500

    Cloudinary img upload

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import moviesRouter from './routes/movies-router.js'
 import userRouter from './routes/user-route.js'
 import seriesRouter from './routes/series-route.js'
 import imageRouter from './routes/image-route.js'
+import imageCRouter from "./routes/imageC-route.js"
 import dotenv from "dotenv"
 import mongoose from "mongoose"
 import path from 'path';
@@ -37,5 +38,6 @@ app.use("/api/v1/movies", moviesRouter)
 app.use("/api/v1/auth", userRouter)
 app.use("/api/v1/series", seriesRouter)
 app.use("/api/v1/uploadImage", imageRouter)
+app.use("/api/v1/Image", imageCRouter)
 
 app.listen(port)
