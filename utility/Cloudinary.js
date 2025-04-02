20a27851a76d4000138997c969d47482d907fc95
haideraly001 <haideralymughal@gmail.com>
2025-04-03 04:39:27 +0500

    Readme

--- utility/Cloudinary.js
+++ utility/Cloudinary.js
@@ -2,15 +2,12 @@ import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 import multer from 'multer';
 
-
-
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAM,
   api_key: process.env.API_KEY,
   api_secret: process.env.API_SECRET
 
 });
-
 
 const storage = new CloudinaryStorage({
   cloudinary,
